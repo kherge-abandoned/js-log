6a8a1e01a317bb9dacbf470d2459639593e2d850
Kevin Herrera <kevin@herrera.io>
2013-07-18 15:35:30 -0700

    Adding configuration files.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig(
+        {
+            qunit: {
+                "logjs": ["qunit.html"]
+            },
+            uglify: {
+                options: {
+                    compress: true,
+                    mangle: true,
+                    sourceMap: "build/log.min.js.map",
+                    banner: "/**\n * Log - A simple log manager.\n * &copy; 2013 Kevin Herrera.\n * https://github.com/herrera-io/js-log/\n */"
+                },
+                "logjs": {
+                    files: {
+                        "build/log.min.js": [
+                            "src/lib/Herrera.js",
+                            "src/lib/Herrera/Log.js",
+                            "src/lib/Herrera/Log/Entry.js",
+                            "src/lib/Herrera/Log/Entry/Attribute.js",
+                            "src/lib/Herrera/Log/Entry/Attributes.js"
+                        ]
+                    }
+                }
+            }
+        }
+    );
+
+    grunt.loadNpmTasks("grunt-contrib-qunit");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+};
